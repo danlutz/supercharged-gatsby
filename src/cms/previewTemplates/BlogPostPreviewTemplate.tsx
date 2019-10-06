1f8cdbdc9a23f1be5bbab94a1603ebbf32b08fd3
Daniel <daniel-lutz@live.at>
2019-10-06 14:37:49 +0200

    Add esModuleInterop flag to tsconfig to allow easy React library imports

--- src/cms/previewTemplates/BlogPostPreviewTemplate.tsx
+++ src/cms/previewTemplates/BlogPostPreviewTemplate.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import { CloudinaryContext } from 'cloudinary-react'
 
 import { BlogPostTemplate } from '../../templates/BlogPostTemplate'
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import { CloudinaryContext } from 'cloudinary-react'
 
 import { SEODefaults } from '../components/SEO'
